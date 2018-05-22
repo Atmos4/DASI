c89f4618b7a05fc53ff48c4fb477258766974b14
gbailly <gbailly@IF501-202-16.insa-lyon.fr>
2018-05-22 03:15:10 +0200

    Bouton "voyance"

--- src/main/java/Serializer/PrinterRecupererDemandeVoyance.java
+++ src/main/java/Serializer/PrinterRecupererDemandeVoyance.java
@@ -5,6 +5,9 @@
  */
 package Serializer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import models.Voyance;
@@ -18,6 +21,15 @@ public class PrinterRecupererDemandeVoyance extends Printer{
     @Override
     public void execute(PrintWriter out, HttpServletRequest request) {
         Voyance v = (Voyance)request.getAttribute("voyance");
+         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject obj = new JsonObject();
+        obj.addProperty("nomClient", v.getClient().getNom());
+        obj.addProperty("prenomClient", v.getClient().getPrenom());
+        obj.addProperty("nomMedium",v.getMedium().getNom());
+        
+        JsonObject container = new JsonObject();
+        container.add("demande",obj);
+        out.println(gson.toJson(container));
         
     }
     
